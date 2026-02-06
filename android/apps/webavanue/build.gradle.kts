82af3b7b3903d268781b5b28df13ac848eeeef49
Naveen Kumar <naveen.kumar@drishinfo.com>
2026-02-06 16:28:16 +0530

    fix(Voiceoscore): Fixed compile issues in build file.

--- android/apps/webavanue/build.gradle.kts
+++ android/apps/webavanue/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.augmentalis.webavanue"
-        minSdk = 28
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
